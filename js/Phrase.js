719af4e6404e5dad5b6d4ff1eff1dd3cff8305cb
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-07 19:57:42 -0400

    begin Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Phrase.js */
+ * Phrase.js 
+
+  * Create the Phrase Class
+  * - include a constructor that receives a phrase parameter and initializes the following properties:
+  * -- addPhraseToDisplay()
+  * -- checkLetter()
+  * -- showMatchedLetter()
+*/
+
+class Phrase {
+  constructor(phrase) {
+    //phrase is actual phrase... converted to all lower case
+    this.phrase = phrase.toLowerCase()
+  }
+}
