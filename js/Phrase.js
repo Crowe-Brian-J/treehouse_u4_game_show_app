424ff9991f379646a21e594f281cca4ab567089d
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-07 20:16:31 -0400

    addPhraseToDisplay() method add

--- js/Phrase.js
+++ js/Phrase.js
@@ -14,4 +14,31 @@ class Phrase {
     //phrase is actual phrase... converted to all lower case
     this.phrase = phrase.toLowerCase()
   }
+
+  //display phrase on game board - adds letter placeholders to display when game starts
+  //each letter presented as empty box
+  // -one li element for each letter
+  // -one li element for each space
+  addPhraseToDisplay = () => {
+    //get phrase div's ul
+    const phraseUl = document.querySelector('#phrase ul')
+    //clear any existing phrase
+    phraseUl.innerHTML = ''
+
+    //Prettier doesn't like spread syntax of [...this.phrase as array here - assign to variable]
+    const characters = this.phrase.split('')
+    //loop over characters to create html for game board
+    characters.forEach((char) => {
+      const li = document.createElement('li')
+      //check space
+      if (char === ' ') {
+        li.className = 'space'
+        //otherwise letter
+      } else {
+        li.className = `hide letter ${char}`
+        li.textContent = char
+      }
+      phraseUl.appendChild(li)
+    })
+  }
 }
