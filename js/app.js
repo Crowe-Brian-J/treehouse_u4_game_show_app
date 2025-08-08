f364615ce16c309094c52015dbee112e8baa2988
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-07 21:16:56 -0400

    app.js file event listeners addition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//global variable
+let game
+
+//get button ids
+const startButton = document.getElementById('btn__reset')
+const qwerty = document.getElementById('qwerty')
+
+//start game with button click
+startButton.addEventListener('click', () => {
+  game = new Game()
+  game.startGame()
+})
+
+//onscreen keyboard click
+qwerty.addEventListener('click', (e) => {
+  //ensure click is on a button element
+  if (e.target.tagName === 'BUTTON') {
+    game.handleInteraction(e.target)
+  }
+})
